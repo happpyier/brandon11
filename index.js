8246c90e62de63f2406c829526ee1893fc1ae1a0
happpyier <happpyier@gmail.com>
2018-01-11 14:05:09 -0500

    setting up files

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express'); 
+var app = express();
+var pg = require('pg');
+const https = require('https');
+const fs = require('fs');
+var path = require("path");
+var url = require("url");
+
+app.get([''], function(request, response) {
+	fs.readFile('index.html', 'utf8', function (err,data) {
+		response.write(data);
+	});
+});
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port')); 
+});
